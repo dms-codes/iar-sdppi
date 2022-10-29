12e258bf7df7ffa0f0d6d6ed92a51aeaf902dbbc
Donny Marthen Sitompul <donnymarthen.sitompul@gmail.com>
2022-10-30 05:10:28 +0700

    Update iar.py
    instead of selenium, we here now are using requests

--- iar.py
+++ iar.py
@@ -1,36 +1,38 @@
-from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup as bs
+import string
 
-def getIARInfo(callsign):
-    callsign = str.upper(callsign)
-    data = {}
-    driver = webdriver.Chrome()
-    driver.get("https://iar-ikrap.postel.go.id/")
-    element = driver.find_element_by_name("Iar[callsign]")
-    element.send_keys(callsign)
-    driver.find_element_by_xpath("//button[@class='btn btn-white btn-raised btn-fab btn-round']").click()
-    tds = driver.find_elements_by_tag_name('td')
+def get_data(callsign):
+    serviceurl = "https://iar-ikrap.postel.go.id/registrant/searchDataIar/?callsign="+callsign
+    payload = {
+               'callsign':callsign,
+              }
+    html = requests.post(serviceurl, data=payload).content
+    soup = bs(html, 'html.parser')
+    res = soup.text
+    for tag in ['"','<\/ul>','\\n','<\/i>','<\/li>','<\/div>','<\/ul>"','                                              ','                                                                                            ']:
+        res = res.replace(tag,'')
+        res = res.lstrip()
+        res = res.rstrip()
+    _, status = res.split('Status : ')
+    _, tingkat = _.split('Tingkat (License Class) : ')
+    _, masalaku = _.split('Masa Laku (Date of expiration) : ')
+    _, callsign = _.split('Tanda Panggilan (Callsign) : ')
+    _, provinsi = _.split('Provinsi (Province) : ')
+    _, nama = _.split('Nama Pemilik (Full name) : ')
+    nama,provinsi,callsign,masalaku,tingkat,status = nama.rstrip(),provinsi.rstrip(),callsign.rstrip(),masalaku.rstrip(),tingkat.rstrip(),status.rstrip()
+    print(f'{callsign},{nama},{provinsi},{masalaku},{tingkat},{status}')
 
-    for i,td in enumerate(tds):
-        if td.text:
-            print(i,td.text)
-
-    data[tds[3].text] = tds[5].text
-    data[tds[9].text] = tds[11].text
-    data[tds[12].text] = tds[14].text
-    data[tds[15].text] = tds[17].text
-    data[tds[18].text] = tds[20].text
-    data[tds[21].text] = tds[23].text
-
-    res = ""
-    for k,v in data.items():
-        res = res + k + ": " + v +"\n"
-
-    driver.close()
-    driver.quit()
-    if len(res) < 10:
-        res = "Not found."
-    return(res)
-
-
+def run():
+    for a in string.ascii_lowercase:
+        for b in string.ascii_lowercase:
+            for c in string.ascii_lowercase+' ':
+                for d in string.ascii_lowercase+' ':
+                    try:
+                        get_data('yb0'+a+b+c+d)
+                    except:
+                        pass  
+                    
 if __name__ == '__main__':
-    print(getIARInfo('YC0DMS'))
+    run()
+  
